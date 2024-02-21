215cca67b6b6a51c2329ef3d6d2f0265abf96b50
Ranga Ngwerume <ranga.ngwerume@codacy.com>
2024-02-21 12:18:53 +0200

    added more tests

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,9 +2,28 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class HelloWorldTest {
-
+    
     @Test
-    public void testGetMessage() {
-        assertEquals("Hello, World!", HelloWorld.getMessage());
+    public void testGuessContinent_ExistingLocation() {
+        String location = "United States";
+        String expectedContinent = "North America";
+        String actualContinent = HelloWorld.guessContinent(location);
+        assertEquals(expectedContinent, actualContinent);
+    }
+    
+    @Test
+    public void testGuessContinent_NonExistingLocation() {
+        String location = "Japan";
+        String expectedContinent = "Sorry, I have no idea where that is.";
+        String actualContinent = HelloWorld.guessContinent(location);
+        assertEquals(expectedContinent, actualContinent);
+    }
+    
+    @Test
+    public void testGuessContinent_CaseInsensitive() {
+        String location = "australia";
+        String expectedContinent = "Australia";
+        String actualContinent = HelloWorld.guessContinent(location);
+        assertEquals(expectedContinent, actualContinent);
     }
 }
