68510b2664dc289a3614351b9c2686f32d2c047a
Ranga Ngwerume <ranga.ngwerume@codacy.com>
2024-02-21 12:03:29 +0200

    updated functionality

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,42 @@
+import java.util.Scanner;
+
 public class HelloWorld {
+    
     public static String getMessage() {
         return "Hello, World!";
     }
-
+    
+    public static String guessContinent(String location) {
+        // List of countries and continents (for demonstration purposes)
+        String[][] countriesAndContinents = {
+            {"United States", "North America"},
+            {"Canada", "North America"},
+            {"Brazil", "South America"},
+            {"France", "Europe"},
+            {"Germany", "Europe"},
+            {"India", "Asia"},
+            {"China", "Asia"},
+            {"Australia", "Australia"},
+            // Add more countries and continents as needed
+        };
+        
+        for (String[] countryContinent : countriesAndContinents) {
+            if (countryContinent[0].equalsIgnoreCase(location)) {
+                return countryContinent[1];
+            }
+        }
+        
+        return "Sorry, I have no idea where that is.";
+    }
+    
     public static void main(String[] args) {
         System.out.println(getMessage());
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Where are you from?");
+        String location = scanner.nextLine();
+        
+        String continent = guessContinent(location);
+        System.out.println("You are on " + continent);
     }
 }
